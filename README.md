462824a9eae856ea8ec39c102695d132c0d5464c
Ilya Shaisultanov <ilya.shaisultanov@gmail.com>
2019-06-23 19:30:08 -0700

    Add network stressor

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Interesting Graphs
 
-Make your test graphs look nice by consuming random amounts of CPU:
+Make your test graphs look nice by consuming random amounts of CPU/memory/io/network:
 
 ```
 docker run --rm diversario/interesting-graphs:0.0.1 150000 300000
--- scripts/all.sh
+++ scripts/all.sh
@@ -3,15 +3,15 @@ set -x
 # continuously use random amount of CPU with random 0 to 5 second sleep in between
 
 while true; do
-  timeout=$(shuf -i 1-3 -n 1)
   memory_timeout=$(shuf -i 1-3 -n 1)
   fs_timeout=$(shuf -i 1-10 -n 1)
-  cpu_timeout=$(shuf -i 1-5 -n 1)
+  network_timeout=$(shuf -i 1-10 -n 1)
   sleep=$(shuf -i 1-10 -n 1)
 
   echo "
 stress-ng --class io --all 1 --timeout $fs_timeout
 stress-ng --class memory --all 1 --timeout $memory_timeout
+stress-ng --class network --all 1 --timeout $network_timeout
 " > stress-commands
 
   parallel -j 2 < stress-commands > /dev/null
